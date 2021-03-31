d94b7fdb3855c7d2143b97a81208df0fc0da2542
Tihomir Bozhkov <tihomir_1998@abv.bg>
2021-03-31 14:03:32 +0300

    Update Week1.R

--- Week1.R
+++ Week1.R
@@ -15,3 +15,43 @@ sd(normData) #Standard Deviation
 
 
 cutoff<-median(normData)+3*mad(normData)
+
+### Task 2 ###
+
+p=runif(1000,0,1)>9/10;
+simData=rnorm(1000,0+(10-0)*p,sqrt(4)+(sqrt(9)-sqrt(4))*p)
+
+hist(simData,prob=TRUE,xlab = 'Observations')
+curve(dnorm(x, mean=mean(simData), sd=sd(simData)), add=TRUE, col=2,lwd=2)
+d=density(simData,bw="SJ")
+lines(d,col='blue',lwd=2)
+
+any(abs((simData-median(simData)))>3*mad(simData))  
+
+### Task 3 ###
+
+a<-c(8,3,8,7,15,9,12,4,9,10,5,1)
+matrr<-matrix(a,6,2) # create matrix 6x2
+matrr
+rownames(matrr)<-c("r1","r2","r3","r4","r5","r6") # named rows
+matrr 
+coll=seq(1,12,2) # create a vector from 1 to 12 with step 2 
+# so this seq. contains odd numbers 
+matr2=cbind(matrr,coll) 
+matr2  #create matrix 6x3 where the last column is named
+
+matr2=cbind(matrr,seq(1,12,2)) # create a matrix 6x3 where thee 3column is without name
+matr2
+ord=order(matr2[,1]) 
+matr2[ord,] # sorting   by 1 coloumn
+
+### Task 4 ###
+
+rand.Circle <- function(n,x_0=0,y_0=0,r=1){
+  theta = 2*pi*runif(n,0,1);
+  R = r*sqrt(runif(n,0,1));
+  x = x_0 + R*cos(theta);
+  y = y_0 + R*sin(theta);
+  return(cbind(x,y))
+}
+plot(rand.Circle(1000,0,0,4),asp=1)
